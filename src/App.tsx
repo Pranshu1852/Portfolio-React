22fe86fcbcf1bc9462a8a27c4a23a9585d633ce5
Pranshu Patel <pranshu.patel@simformsolutions.com>
2025-05-29 11:29:42 +0530

    Add: create error boundry to show fallback component when error occur

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
+import { ErrorBoundary } from 'react-error-boundary';
 import { Routes, Route } from 'react-router-dom';
 
-import NotFound from './features/About/components/NotFound';
+import ErrorFallBack from './components/ErrorFallback';
+import NotFound from './components/NotFound';
 import About from './features/About/pages/About';
 import Contact from './features/Contact/pages/Contact';
 import ProjectDetail from './features/Projects/pages/ProjectDetail';
 import Projects from './features/Projects/pages/Projects';
 import MainLayout from './layouts/MainLayout';
+import { sharedRef } from './utils/sharedRef';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<MainLayout />}>
-        <Route index element={<About />} />
-        <Route path='/projects' element={<Projects />} />
-        <Route path='/projects/:id' element={<ProjectDetail />} />
-        <Route path='/contact' element={<Contact />} />
-      </Route>
-      <Route path='/*' element={<NotFound />} />
-    </Routes>
+    <ErrorBoundary
+      fallbackRender={ErrorFallBack}
+      onReset={() => {
+        if (sharedRef.current) {
+          sharedRef.current.navigate('/');
+        }
+      }}
+    >
+      <Routes>
+        <Route path='/' element={<MainLayout />}>
+          <Route index element={<About />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/projects/:id' element={<ProjectDetail />} />
+          <Route path='/contact' element={<Contact />} />
+        </Route>
+        <Route path='/*' element={<NotFound />} />
+      </Routes>
+    </ErrorBoundary>
   );
 }
 
